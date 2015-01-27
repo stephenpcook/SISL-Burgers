713f9d24267840e8883b8688f908159ed008156e
Stephen P. Cook <s.cook@bath.ac.uk>
2015-01-27 13:56:31 +0000

    burgers.m allows non-zero boundary conditions
    Added the term u_l and u_r to many parts of the code to allow
    for non-zero boundary conditions.
    - A vector BC was added to allow for corrections to u_xx approximation
    - This term was added to the interpolated rhs_D and  subtracted before
      matrix division by lhs_A
    - u_l and u_r was added to the approximation to u_xx at x_0 and x_r
    There were also many mistakes fixed:
    - The sign on del2 was wrong, but the minus was put in the following
      line
    - The estimate to the second derivative at x0 and x1 had a sign wrong
    - Small corrections to plotting and commenting.
    - Modified to allow easy changes to x0, x1, t0 and tmax.

--- burgers.m
+++ burgers.m
@@ -1,19 +1,22 @@
-% Solving Burgers with a SL method.
+% Solving Burgers with a SL method, uniform grid.
 clear all
 clf
 
 % Parameters
 N=21;       % Level of spacial discretisation
-Dx= 1./(N+1); % Space step
 tN = 200;
 %Dt= 0.012;   % Timestep
 theta_t=1/2;  % Theta for the Theta method in time.
 theta_x=1/2;  % Theta for the Theta-method for departure points.
 epsilon=0.002; % Epsilon in the PDE
+
+x0 = 0;
+x1 = 1;
+Dx= (x1 - x0)./(N+1); % Space step
+
+t0 = 0;
 tmax = 1.5;
-
-Dt = tmax/tN;
-
+Dt = (tmax-t0)/tN;
 
 % Initial and boundary conditions
 u0 = @(x) sin(2*pi*x) + 1/2*sin(pi*x);
@@ -24,32 +27,34 @@ u_r = 0;
 
 % del2 - \delta_x^2, a tridiagonal matrix, finite difference second 
 % derivative operator.
-del2 = 2*eye(N) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
-% LHS and RHS of SISL formulation
-M_RHS = eye(N) - Dt/(Dx^2) * (1 - theta_t) * epsilon * del2;
-M_LHS = eye(N) + Dt/(Dx^2) * theta_t * epsilon * del2;
+del2 = -2*eye(N) + diag(ones(N-1,1),1) + diag(ones(N-1,1),-1);
+% LHS and RHS of SISL formulation, and the boundary term correction
+M_RHS = eye(N) + Dt/(Dx^2) * (1 - theta_t) * epsilon * del2;
+M_LHS = eye(N) - Dt/(Dx^2) * theta_t * epsilon * del2;
+BC = [u_l/(Dx^2); zeros(N-2,1);u_r/(Dx^2)];
 
 % Initialisation
-X = (1:N)'./(N+1);
+X = x0 + (1:N)'*Dx;
 Un = u0(X);
 
 % For plotting...
-tout=(0:Dt:tmax)';
+tout = t0 + (0:tN)*Dt;
 uout=zeros(length(tmax),N);
 uout(1,:) = Un;
 xout = X;
 jj=1;
 
 % Outer loop. Timestep
-for t = 0:Dt:(tmax-Dt)
+for ii = 1:(length(tout))
 % Timestep initialisation
 % Cubic spline of Un (including the constant end points)  
-rhs0 = -Dt/(Dx^2) * (1-theta_t) * epsilon *...
-    (5*Un(1) + 4*Un(2) - Un(3)); % This is an order h^2 approx'n to u''
-rhsN = -Dt/(Dx^2) * (1-theta_t) * epsilon *...
-    (5*Un(N) + 4*Un(N-1) - Un(N-2));
+rhs0 = u_l + Dt/(Dx^2) * (1-theta_t) * epsilon *...
+    (2*u_l - 5*Un(1) + 4*Un(2) - Un(3)); % This is an order h^2 approx'n to u''
+rhsN = u_r + Dt/(Dx^2) * (1-theta_t) * epsilon *...
+    (2*u_r - 5*Un(N) + 4*Un(N-1) - Un(N-2));
 %pp_rhs = spline([0;X;1],[rhs0; (M_RHS * Un) ; rhsN]);
-pp_rhs = interp1([0;X;1],[rhs0; (M_RHS * Un) ; rhsN],'linear','pp');
+pp_rhs = interp1([x0;X;x1],...
+  [rhs0; (M_RHS * Un + Dt*(1-theta_t)*epsilon*BC) ; rhsN],'linear','pp');
 %   TODO
 %   This is actually wrong at the moment. Want to take into account
 %   the second derivative term at the left and right points.
@@ -63,21 +68,20 @@ X_D = X - Dt*Un;
  % Evaluate RHS at the departure points.
  rhs_D = ppval(pp_rhs, X_D);
  % Solve the implicit equation (Thomas algorithm implemented later)
- U_A = M_LHS\rhs_D;
+ U_A = M_LHS\(rhs_D + Dt*theta_t*epsilon*BC);
  % New guess at the departure points (theta-method) and recalculate 
  % RHS_D and U_A. 
  X_D = X - Dt*(theta_x*U_A + (1-theta_x)*Un);
- rhs_D = ppval(pp_rhs, X_D);
  end
- U_A = M_LHS\rhs_D;
+ rhs_D = ppval(pp_rhs, X_D);
+ U_A = M_LHS\(rhs_D + Dt*theta_t*epsilon*BC);
  % End of Inner Loop
 % Prep for next timestep.
 Un = U_A;
-%plot([u_l;U_A;u_r]), title(['t = ',num2str(t)]), ylim([-1 1.5]), drawnow()
+plot([x0;X;x1],[u_l;U_A;u_r])
+title(['t = ',num2str(tout(ii))]), ylim([-1 1.5]), drawnow()
 jj=jj+1;
 uout(jj,:)=Un;
-end % for t
+end % for ii
 
 % mk_video('test.avi','test',tout,uout,xout);
-
-

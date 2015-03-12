6df92bc05afd6550181cfe6d649d0a09a7d578a5
Stephen P. Cook <s.cook@bath.ac.uk>
2015-03-12 16:46:03 +0000

    Modified burg2.m to allow for non-zero BCs
    Changed the default behaviour to represent the travelling wave solution.

--- burg2.m
+++ burg2.m
@@ -3,25 +3,45 @@
 % Uniform grid, linear interpolation, Crank-Nicholson-like Departure points
 
 clear all
-N = 101;
-Nt = 1500;
+
+% Parameters
+N = 300;
+Nt = 3000;
 K = 4;
 
-Dx = 1/(N+1);
-Dt = 2/Nt;
-u0 = @(x) sin(2*pi*x) + 1/2*sin(pi*x);
+t0 = 0;
+tmax = 1;
 epsilon = 0.002;
 alpha = 0.5;
 
-X_A = (Dx:Dx:(1-Dx))';
-X_A_bc = (0:Dx:1)';
+%x_l = 0;
+%x_r = 1;
+%u0 = @(x) sin(2*pi*x) + 1/2*sin(pi*x) - 0.5;
+%plotlims = [-2.5 2.5];
+x_l = -1;
+x_r = 4;
+c = 1;
+alpha_ = 0.5;
+u0 = @(x) c - alpha_*tanh(alpha/(2*epsilon)*(x - c*t0));
+plotlims = [0 1.6];
+
+% Setup
+Dx = (x_r-x_l)/(N+1);
+Dt = tmax/Nt;
+u_l = u0(x_l);
+u_r = u0(x_r);
+
+X_A_bc = (x_l:Dx:x_r)';
+X_A = X_A_bc(2:end-1);
 
 delta2 = 1/(Dx^2) * ...
     (-2*eye(N) + diag(ones(N-1,1),1) + diag(ones(N-1,1),-1));
 A = eye(N) - Dt*epsilon*alpha*delta2;
 Ainv = A^(-1);
 B = eye(N) + Dt*epsilon*(1-alpha)*delta2;
+C = Dt*epsilon/(Dx^2)*[u_l;zeros(N-2,1);u_r];
 
+% Initialisation
 Un = u0(X_A);
 U_out = zeros(N,Nt);
 X_D_out = zeros(N,Nt,K);
@@ -34,11 +54,11 @@ for tt = 1:Nt
       X_D_old = X_D;
       X_D = X_A - Dt*Unplus1;
       for ll = 1:2
-        Un_D = interp1(X_A_bc,[0;Un;0],X_D);
+        Un_D = interp1(X_A_bc,[u_l;Un;u_r],X_D);
         X_D = X_A - Dt*(0.5*Unplus1 + 0.5*Un_D);
       end
-      R_D = interp1(X_A_bc,[0;B*Un;0],X_D);
-      Unplus1 = Ainv*R_D;
+      R_D = interp1(X_A_bc,[u_l;B*Un + (1-alpha)*C;u_r],X_D); % No u_xx on x_l, x_r
+      Unplus1 = Ainv*(R_D + alpha*C);
       X_D_out(:,tt,kk) = X_D;
       X_D_diff = X_D - X_D_old;
     end % for kk
@@ -47,8 +67,8 @@ for tt = 1:Nt
 end % for tt
 
 for tt = 1:Nt
-plot(X_A_bc,[0;U_out(:,tt);0])
-ylim([-2,2])
+plot(X_A_bc,[u_l;U_out(:,tt);u_r])
+ylim(plotlims)
 drawnow
 end
 

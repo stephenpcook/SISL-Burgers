7e095837c09b30c85575ec2b75a176eafb9acd19
Stephen P. Cook <s.cook@bath.ac.uk>
2015-09-19 11:48:08 +0100

    Reordered parameter definitions

--- burg2.m
+++ burg2.m
@@ -3,51 +3,54 @@ function [Un,X_A] = burg2(N,Nt)
 %
 % Uniform grid, linear interpolation, Crank-Nicholson-like Departure points
 
-%clear all
-
-% Parameters
+% General Parameters
 %N = 300;
 %Nt = 3000;
 K = 40;
+theta_x = 0.5;
+epsilon = 0.0001;
+
+c = 1;
+alpha_0 = 0.1;
+
+% Domain parameters
+x_l = -1;
+x_r = 4;
 
 t0 = 0;
 tmax = 1.5;
-epsilon = 0.0001;
-alpha_0 = 0.5;
+
+% Initial and Boundary Conditions
 
 %x_l = 0;
 %x_r = 1;
 %u0 = @(x) sin(2*pi*x) + 1/2*sin(pi*x) - 0.5;
-%plotlims = [-2.5 2.5];
-x_l = -1;
-x_r = 4;
-c = 1;
-alpha_ = 0.1;
-u0 = @(x) c - alpha_*tanh(alpha_/(2*epsilon)*(x - c*t0));
+
+u0 = @(x) c - alpha_0*tanh(alpha_0/(2*epsilon)*(x - c*t0));
+u_l = u0(x_l);
+u_r = u0(x_r);
 
 %tmax = 15;
 %x_l = -10;
 %x_r = 40;
-%u0 = @(x) c - alpha_*tanh(alpha_/(2*epsilon)*(x - c*t0));
 
-
+% Plot parameters
+%plotlims = [-2.5 2.5];
 plotting = 0;
 plotlims = [0 1.6];
 
 % Setup
 Dx = (x_r-x_l)/(N+1);
 Dt = tmax/Nt;
-u_l = u0(x_l);
-u_r = u0(x_r);
 
 X_A_bc = (x_l:Dx:x_r)';
 X_A = X_A_bc(2:end-1);
 
 delta2 = 1/(Dx^2) * ...
     (-2*eye(N) + diag(ones(N-1,1),1) + diag(ones(N-1,1),-1));
-A = eye(N) - Dt*epsilon*alpha_0*delta2;
+A = eye(N) - Dt*epsilon*theta_x*delta2;
 Ainv = A^(-1);
-B = eye(N) + Dt*epsilon*(1-alpha_0)*delta2;
+B = eye(N) + Dt*epsilon*(1-theta_x)*delta2;
 C = Dt*epsilon/(Dx^2)*[u_l;zeros(N-2,1);u_r];
 
 % Initialisation
@@ -70,10 +73,10 @@ for tt = 1:Nt
         X_D(X_D<x_l) = x_l;
         X_D(X_D>x_r) = x_r;
       end
-      R_D = interp1(X_A_bc,[u_l;B*Un + (1-alpha_0)*C;u_r],X_D); % No u_xx on x_l, x_r
-      %ENO_pp = interp_ENO(X_A_bc,[u_l;B*Un + (1-alpha_0)*C;u_r]);
+      R_D = interp1(X_A_bc,[u_l;B*Un + (1-theta_x)*C;u_r],X_D); % No u_xx on x_l, x_r
+      %ENO_pp = interp_ENO(X_A_bc,[u_l;B*Un + (1-theta_x)*C;u_r]);
       %R_D = ppval(ENO_pp, X_D);
-      Unplus1 = Ainv*(R_D + alpha_0*C);
+      Unplus1 = Ainv*(R_D + theta_x*C);
       X_D_out(:,tt,kk) = X_D;
       X_D_diff = X_D - X_D_old;
     end % for kk

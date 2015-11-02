2204ed32ece87654ec29509e2b0571cd6fa48073
Stephen P. Cook <s.cook@bath.ac.uk>
2015-11-02 18:27:09 +0000

    New experiments for alan and option file
    modified:   experiments/alan/gen_alan.m
                options/interpolation/gen_interpolation.m
    
    gen_alan.m creates:
     - alan0.mat: defaults
     - alan6.mat: cubic lagrange, no limiter
     - alan7.mat: m = sqrt(0.1 + u_xx^2)
     gen_interpolation creates:
     - cubic_lagrange_no_limiter.mat

--- experiments/alan/gen_alan.m
+++ experiments/alan/gen_alan.m
@@ -3,33 +3,66 @@ bigNT = [40;80;160];
 bigNX = [(20:320),(325:5:640),(650:10:1000)];
 program_name = 'burgersSLMM';
 
+%% alan0
+% Defaults
+out_filename = 'experiments/alan/alan0_out.mat';
+save('alan1.mat',...
+    'bigNT', 'bigNX', 'out_filename',...
+    'program_name');
+
+%% alan1
+% alpha = 0.25
 out_filename = 'experiments/alan/alan1_out.mat';
 param_file = 'alpha25.mat';
 save('alan1.mat',...
     'bigNT', 'bigNX', 'out_filename',...
     'param_file', 'program_name');
 
+%% alan2
+% alpha = 0.5
 out_filename = 'experiments/alan/alan2_out.mat';
 param_file = 'alpha5.mat';
 save('alan2.mat',...
     'bigNT', 'bigNX', 'out_filename',...
     'param_file', 'program_name');
 
-
+%% alan3
+% thetaX = thetaU = 0.51
 out_filename = 'experiments/alan/alan3_out.mat';
 param_file = 'theta51.mat';
 save('alan3.mat',...
     'bigNT', 'bigNX', 'out_filename',...
     'param_file', 'program_name');
 
+%% alan4
+% moving mesh, m = sqrt(0.1 + u_x^2)
 out_filename = 'experiments/alan/alan4_out.mat';
 param_file = 'moving_exact_arc.mat';
 save('alan4.mat',...
     'bigNT', 'bigNX', 'out_filename',...
     'param_file', 'program_name');
 
+%% alan5
+% Clagrange + limiter
 out_filename = 'experiments/alan/alan5_out.mat';
 param_file = 'only_cubic_lagrange.mat';
 save('alan5.mat',...
+    'bigNT', 'bigNX', 'out_filename',...
+    'param_file', 'program_name');
+
+%% alan6
+% Clagrange (no limiter)
+out_filename = 'experiments/barry/alan6_out.mat';
+param_file = 'cubic_lagrange_no_limiter.mat';
+
+save('alan6.mat',...
+    'bigNT', 'bigNX', 'out_filename',...
+    'param_file', 'program_name');
+
+%% alan7
+% moving mesh, m = sqrt(0.1 + u_xx^2)
+out_filename = 'experiments/alan/alan7_out.mat';
+param_file = 'moving_exact_curv.mat';
+save('alan7.mat',...
     'bigNT', 'bigNX', 'out_filename',...
     'param_file', 'program_name');
--- options/interpolation/gen_interpolation.m
+++ options/interpolation/gen_interpolation.m
@@ -1,5 +1,12 @@
 clear all
+%% only_cubic_lagrange
 interpolation = 'CLagrange';
 limiter = 1;
 save('only_cubic_lagrange',...
+    'interpolation', 'limiter');
+
+%% cubic_lagrange_no_limiter
+interpolation = 'CLagrange';
+limiter = 0;
+save('cubic_lagrange_no_limiter',...
     'interpolation', 'limiter');

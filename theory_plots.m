fbd11d6366aee73200d8a3518954907049b96734
Stephen P. Cook <s.cook@bath.ac.uk>
2017-03-24 14:47:40 +0000

    Plot theoretical front speed and set defaults
    In some cases certain variables were not defined for all useful
    settings. Allowing for these defaults to be created allows this code to
    be run, e.g. using
    
    ```
    theory_plots('alan0.mat')
    ```
    
    where `alan0.mat` is created by `experiments/alan/gen_alan.m`.
    
    It may be a good idea to throw warnings when these are used.

--- theory_plots.m
+++ theory_plots.m
@@ -6,18 +6,42 @@ end
 
 load(in_filename)
 
+if ~exist('BigNX','var')
+    [BigNT, BigNX] = meshgrid(bigNT,bigNX);
+end
+if ~exist('c','var')
+    c = 1;
+end
+if ~exist('alpha_0','var')
+    alpha_0 = 0.1;
+end
+if ~exist('epsilon','var')
+    epsilon = 1e-4;
+end
+
 Dx = 5./BigNX;
 Dt = 1.5./BigNT;
 
 BigEll = floor(c*Dt./Dx);
 epsmin = alpha_0*Dx./4;
 
-BigEpsHat = epsilon + 0.5*((2*BigEll+1).*Dx - Dt)...
+% Equation eq:eps_hat in PaperNew.tex
+BigEpsHat = epsilon + 0.5*((2*BigEll+1).*Dx - c*Dt)*c...
     - (BigEll.^2 + BigEll).*(Dx.^2./(2*Dt));
 BigEpsTheo = max(BigEpsHat, epsmin);
+
+% Equation eq:c_hat_theory in PaperNew.tex
+BigCHatTheo = c - alpha_0^2./(6*BigEpsTheo)...
+    .*((2*BigEll+1).*Dx - 2*c*Dt);
 
 h1 = figure;
 semilogy(BigNX, BigEpsTheo);
 xlabel('N_x')
 ylabel('\epsilon_{hat}')
-title('Theoretical \epsilon_{hat}')
+%title('Theoretical \epsilon_{hat}')
+
+h2 = figure;
+plot(BigNX, BigCHatTheo);
+xlabel('N_x')
+ylabel('\c_hat')
+ylim([0.9,1.1])
